fcaba4cfe1b3112dc034abf13fd71c91aea71d70
Royal-Reddy-Yaparla <iamroyalreddy@gmail.com>
2024-04-02 18:14:58 +0530

    variable01 file has been added

---
+++ 02-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Siraj
+PERSON2=Shami
+
+echo "$PERSON1: Hello $PERSON2, How are you"
+echo "$PERSON2: Hi $PERSON1,I am fine, Hope you are doing well"
+echo "$PERSON1: Yeah I'm Great, $PERSON2, Have Done your Practice"
+echo "$PERSON2: I have done just now $PERSON1"
