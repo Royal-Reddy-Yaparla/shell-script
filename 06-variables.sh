974bed5c8ba3c3497214dc7c1e92863124754a11
Royal-Reddy-Yaparla <iamroyalreddy@gmail.com>
2024-04-02 19:34:13 +0530

    Basic calculation

---
+++ 06-variables.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "we are going add two number"
+NUMBER1=$1
+
+NUMBER2=$2
+
+ADDITION=$((NUMBER1+NUMBER2))
+SUBTRACTION=$((NUMBER1-NUMBER2))
+MULTIPLICATION=$((NUMBER1*NUMBER2))
+DIVISION=$((NUMBER1/NUMBER2))
+
+echo "Addition of $NUMBER1  and $NUMBER2 is $ADDITION"
+echo "Subtraction of $NUMBER1  and $NUMBER2=$SUBTRACTION"
+
+echo "Multiplication of $NUMBER1  and $NUMBER2 is $MULTIPLICATION"
+echo "Divison of $NUMBER1  and $NUMBER2=$DIVISION"
+
+
