5dffed5246113ebdb049943a0de996a10885de73
Royal-Reddy-Yaparla <iamroyalreddy@gmail.com>
2024-04-02 18:21:02 +0530

    argument based variable file has been added

--- 01-hello_world.sh
+++ 01-hello_world.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-echo "Hello world!"
+echo "Siraj: Hello Shami, How are you"
+echo "Shami: Hi Siraj,I am fine, Hope you are doing well"
+echo "Siraj: Yeah I'm Great, Shami, Have Done your Practice"
+echo "Shami: I have done just now Siraj"
