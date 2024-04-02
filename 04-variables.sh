0f8b06d5f0b99f9876c95aca0cdcec0ff0fd33d1
Royal-Reddy-Yaparla <iamroyalreddy@gmail.com>
2024-04-02 18:37:42 +0530

    variable file name has been change

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: Hello $PERSON2, How are you"
+echo "$PERSON2: Hi $PERSON1,I am fine, Hope you are doing well"
+echo "$PERSON1: Yeah I'm Great, $PERSON2, Have Done your Practice"
+echo "$PERSON2: I have done just now $PERSON1"
