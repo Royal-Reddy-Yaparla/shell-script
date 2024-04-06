e3688e23c98f7901c65492fa2baf6dec172649d5
Royal-Reddy-Yaparla <iamroyalreddy@gmail.com>
2024-04-06 16:55:33 +0530

    mysql-git install script

---
+++ 09-package-install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+
+# check if root user or not
+if [ $ID -ne 0 ]
+then 
+    echo "$R Provide root accusses to this script $N"
+    exit 1
+fi
+
+
+VALIDATE(){
+    if [ $? -ne 0 ]
+    then 
+        echo "Installing $1 ... $R FAILED"
+        exit 1
+    else
+        echo "Installing $1 ... $G SUCCESS"
+    fi
+}
+
+
+apt install mysql-server -y
+VALIDATE mysql
+
+apt install git -y
+VALIDATE git
