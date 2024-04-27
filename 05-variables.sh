e29e627011b1785d45ae9c274994c90e2a156939
Yaparla Royal Reddy <106161416+Royal-Reddy-Yaparla@users.noreply.github.com>
2024-04-27 21:42:50 +0530

    Update 05-variables.sh

--- 05-variables.sh
+++ 05-variables.sh
@@ -9,6 +9,9 @@ echo "Enter password"
 stty -echo
 read -r PASSWORD
 
+# read -s PASSWORD
+# we can use -s for hide entries but need ./<file> to excute script
+
 echo "username: $USERNAME password: $PASSWORD"
 
 stty echo
